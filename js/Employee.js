60df1b43d18c9e81c067a3f113ed71088f7830ec
thepartha <45639354+thepartha@users.noreply.github.com>
2019-04-20 12:27:32 -0500

    Updated

---
+++ js/Employee.js
@@ -0,0 +1,12 @@
+class Employee {
+    constructor(name,email,city,phone, address,dob, image){
+        this.name = name;
+        this.email = email;
+        this.city = city;
+        this.phone = phone;
+        this.address = address;
+        this.dob = dob;
+        this.image = image;
+    }
+
+}
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+//GET DOM ELEMENTS
 const body = document.getElementsByTagName('BODY')[0];
 const gallery = document.getElementById('gallery');
 const modal = document.querySelector('.modal-container');
@@ -6,10 +6,12 @@ const searchDiv = document.querySelector('.search-container');
 const searchResultsGallery = document.createElement('div');
 searchResultsGallery.setAttribute('class','gallery')
 modal.style.display = "none";
-let employeeArr = [];
-let index = 0;
-const recordCount = 0;
-//Search HTML
+
+//DECLARE EMPLOYEE ARRAYS
+let employees=[];
+let searchedEmployees=[];
+
+//CREATE AND APPEND SEARCH HTML
 const searchHTML = `
     <form action="#" method="get">
     <input type="search" id="search-input" class="search-input" onkeyup="searchEmployees()" placeholder="Search...">
@@ -17,121 +19,105 @@ const searchHTML = `
     </form>`
 searchDiv.innerHTML = searchHTML;
 
-
-//FETCH FUNCTIONS 
-
+//FETCH FUNCTION
 fetch('https://randomuser.me/api/?format=json&results=12&?nat=gb')
     .then(response => response.json())
     .then(data => generateGallery(data.results));
-  // .then(data => console.log(data));
 
-
-
-//HELPER FUNCTIONS 
-function searchEmployees() {
+//SEARCH EMPLOYESS FUNCTION 
+  function searchEmployees() {
     const searchString = document.getElementById('search-input').value;
-    const cards = gallery.children;
-    searchResultsGallery.innerHTML = '';
+    gallery.innerHTML = '';
     if(searchString) {
-        gallery.classList.add('hidden');
-       for (let i = 0; i < cards.length ; i ++) {
-            if (cards[i].innerText.search(searchString) > -1) { //search each list item for matching string 
-                searchResultsGallery.appendChild(cards[i].cloneNode(true)); //Clone element from the main list and append to Search list 
+       for (let i = 0; i < employees.length ; i ++) {
+            if (employees[i].name.search(searchString) > -1) { //search each list item for matching string 
+               searchedEmployees.push(employees[i]); 
             }      
            }
-        
-        for (let i = 0; i < searchResultsGallery.children.length; i++) {
-            searchResultsGallery.children[i].addEventListener('click',function(event){detail(event, null)});
-        }   
-        body.appendChild(searchResultsGallery);
-        
-           
+           displayGallery(searchedEmployees); 
+           searchedEmployees=[];
+           search = true;
     } else {       
-        if(searchResultsGallery){
-         searchResultsGallery.innerHTML = '';
-      //   searchResultsGallery.parentNode.removeChild(searchResultsGallery);
-        }
-        gallery.classList.remove('hidden');
+        displayGallery(employees);
+        search = false;
     }
 }
 
-
-
-
+//GENERATE GALLERY AND ADD TO THE EMPLOYESS ARRAY 
 function generateGallery(results) {
+    results.forEach((item) => {
+     const employee = new Employee();
+            employee.name = item.name.first + ' ' + item.name.last,
+            employee.email = item.email,
+            employee.city= item.location.city,
+            employee.address= item.location.street + ", " + item.location.state + " " + item.location.postcode,
+            employee.image = item.picture.large,
+            employee.phone = item.cell,
+            employee.dob = item.dob.date.slice(0,10)
+            employees.push(employee);
+      });
+      
+      displayGallery(employees);
+}
 
-    results.forEach((item, i) => {
-      const card = document.createElement('div');
-      card.setAttribute('class','card');
-      card.setAttribute('id', i);
-      const employee = {
-            name : item.name.first + ' ' + item.name.last,
-            email : item.email,
-            city : item.location.city,
-            address : item.location.street + ", " + item.location.state + " " + item.location.postcode,
-            picture : item.picture.large,
-            phone : item.cell,
-            birthday : item.dob.date
-      };
-      card.innerHTML = `
-          <div class="card-img-container">
-              <img class="card-img" src="${item.picture.medium}" alt="profile picture">
-          </div>
-          <div class="card-info-container">
-              <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last} </h3>
-              <p class="card-text">${item.email}</p>
-              <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
-          </div>`;
-      card.addEventListener('click',function(event){detail(event, null)});
-      employeeArr.push(employee);
-      gallery.appendChild(card);
-    });
-    }
+//GENERATE HTML AND DISPLAY GALLERY
+function displayGallery(employees){
+    employees.forEach((employee, index)=>{
+        const card = document.createElement('div');
+        card.setAttribute('class','card');
+        card.innerHTML = `
+           <div class="card-img-container">
+               <img class="card-img" src="${employee.image}" alt="profile picture">
+           </div>
+           <div class="card-info-container">
+               <h3 id="name" class="card-name cap">${employee.name} </h3>
+               <p class="card-text">${employee.email}</p>
+               <p class="card-text cap">${employee.city}</p>
+           </div>`;
+       card.addEventListener('click',function(){displayModal(employees, index)});
+       gallery.appendChild(card);
+    })
 
+}
 
-
-//SHOW MODAL WITH EMPLOYEE DETAIL    
-function detail(event, i){
-    if (i == null) {
-    // GETTING THE ID FOR THE TARGET DIV
-    if (event.target.className == "card-text" || event.target.className == "card-text cap" || event.target.className == "card-name cap" || event.target.className == "card-img") 
-    { index = event.target.parentNode.parentNode.id;
-    } else if (event.target.className == "card-info-container" || event.target.className == "card-img-container") {
-    index = event.target.parentNode.id; }
-    } else {
-        if (i == -1) {
-            index = 11;
-        } else if (i == 12) {
-            index = 0;
-        } else {
-        index = i;
-        }
-  } 
-  const modalHTML = `
-        <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn" onclick="closeModal()"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src=" ${employeeArr[index].picture}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap"> ${employeeArr[index].name}</h3>
-                    <p class="modal-text">${employeeArr[index].email}</p>
-                    <p class="modal-text cap">${employeeArr[index].city}</p>
-                    <hr>
-                    <p class="modal-text">${employeeArr[index].phone}</p>
-                    <p class="modal-text">${employeeArr[index].address}</p>
-                    <p class="modal-text">Birthday: ${employeeArr[index].birthday}</p>
-                </div>
-                <div class="modal-btn-container">
-                <button type="button" id="modal-prev" class="modal-prev btn" onclick="detail(null,${+index - 1})">Prev</button>
-                <button type="button" id="modal-next" class="modal-next btn" onclick="detail(null,${+index + 1})">Next</button>
-            </div>`
+//GENERATE HTML AND DISPLAY MODAL 
+function displayModal(employees, index){
+    const dob = employees[index].dob
+    const modalHTML = `
+    <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn" onclick="closeModal()"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=" ${employees[index].image}" alt="profile picture">
+                <h3 id="name" class="modal-name cap"> ${employees[index].name}</h3>
+                <p class="modal-text">${employees[index].email}</p>
+                <p class="modal-text cap">${employees[index].city}</p>
+                <hr>
+                <p class="modal-text">${employees[index].phone}</p>
+                <p class="modal-text">${employees[index].address}</p>
+                <p class="modal-text">Birthday: ${dob.slice(5,7)}/${dob.slice(8)}/${dob.slice(0,4)}</p>
+            </div>
+            <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>`
     modal.innerHTML = modalHTML;
+    document.getElementById('modal-prev').addEventListener('click', function() {handleClick(employees, index - 1)});    
+    document.getElementById('modal-next').addEventListener('click', function() {handleClick(employees, index + 1)});  
     modal.style.display = "block";
 }
 
+// HANDLE PREVIOUS AND NEXT BUTTONS ON THE MODAL
+function handleClick(employees, index){
+    if(index >= employees.length){
+        index = 0
+    } else if (index < 0) {
+        index = employees.length - 1
+    }
+   displayModal(employees, index)
+}
 
 //CLOSE MODAL    
 function closeModal() {
     modal.style.display = "none";
-    modal.innerHTML = "";
-    
+    modal.innerHTML = ""; 
 };
